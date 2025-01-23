012793aa801d3b2df241e5b6a6e65c6512f2300c
angel sabaleta <136175537+mangel0410@users.noreply.github.com>
2025-01-22 23:32:16 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Configuración de la conexión a MySQL
+const db = mysql.createConnection({
+    host: 'bi2tratauexzwhxfvhvf-mysql.services.clever-cloud.com',
+    user: 'udn4zcbscckqe8hv',
+    password: 'yFZXJjCUiukocAc9aUtk',
+    database: 'bi2tratauexzwhxfvhvf',
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error al conectar a MySQL:', err);
+        return;
+    }
+    console.log('Conexión exitosa a MySQL');
+});
+
+// Endpoint de prueba
+app.get('/api/datos', (req, res) => {
+    db.query('select * from personas', (err, results) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+// Inicia el servidor
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
